7491b7f67b7982d06ef4798cc1d84776b4ce1b1e
Alexander Chadin <joker946@gmail.com>
2015-07-16 13:14:49 +0300

    weight add

--- nova/scheduler/load_balancer.py
+++ nova/scheduler/load_balancer.py
@@ -13,15 +13,16 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
+from keystoneclient.v2_0 import client
 from oslo.config import cfg
+from nova import context as nova_context
 from nova import db
+from nova import image
+from nova import objects
 from nova.i18n import _
 from nova.openstack.common import log as logging
+from nova.openstack.common import importutils
 
-
-CONF = cfg.CONF
-LOG = logging.getLogger(__name__)
 
 lb_opts = [
     cfg.IntOpt('cpu_threshold',
@@ -41,13 +42,25 @@ lb_opts = [
                  help='IO weight')
 ]
 
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
 CONF.register_opts(lb_opts, 'loadbalancer')
+CONF.import_opt('scheduler_host_manager', 'nova.scheduler.driver')
+
+
+glance_username = 'glance'
+glance_password = 'glance'
+tenant_name = 'service'
+auth_url = 'http://controller1:5000/v2.0/'
 
 
 class LoadBalancer(object):
 
     def __init__(self, *args, **kwargs):
         super(LoadBalancer, self).__init__(*args, **kwargs)
+        self.host_manager = importutils.import_object(
+            CONF.scheduler_host_manager)
+        self.image_api = image.API()
 
     def _normalize_params(self, instances):
         max_values = {}
@@ -93,18 +106,34 @@ class LoadBalancer(object):
         cpu_load = round(cpu_load, 2)
         return cpu_load
 
+    def _weight_instances(self, normalized_instances):
+        weighted_instances = []
+        for instance in normalized_instances:
+            weighted_instance = {'uuid': instance['uuid']}
+            weight = CONF.loadbalancer.cpu_weight * instance['cpu'] + \
+                CONF.loadbalancer.memory_weight * instance['memory'] + \
+                CONF.loadbalancer.io_weight * instance['io']
+            weighted_instance['weight'] = weight
+            weighted_instances.append(weighted_instance)
+        return sorted(weighted_instances,
+                      key=lambda x: x['weight'], reverse=True)
+
     def _choose_instance_to_migrate(self, instances, compute_nodes):
         instances_params = []
         for i in instances:
-            if i.instance['vm_state'] == 'active' and i.instance['prev_cpu_time']:
-                instance_weights = {'uuid': i.instance['uuid']}
-                instance_weights['cpu'] = self._calculate_cpu(i)
-                instance_weights['memory'] = i['mem']
-                instance_weights['io'] = i[
-                    'block_dev_iops'] - i['prev_block_dev_iops']
-                instances_params.append(instance_weights)
+            if i.instance['vm_state'] == 'active':
+                if i['prev_cpu_time']:
+                    instance_weights = {'uuid': i.instance['uuid']}
+                    instance_weights['cpu'] = self._calculate_cpu(i)
+                    instance_weights['memory'] = i['mem']
+                    instance_weights['io'] = i[
+                        'block_dev_iops'] - i['prev_block_dev_iops']
+                    instances_params.append(instance_weights)
         normalized_instances = self._normalize_params(instances_params)
         LOG.info(_(normalized_instances))
+        weighted_instances = self._weight_instances(normalized_instances)
+        LOG.info(_(weighted_instances))
+        return weighted_instances
 
     def _step_threshold_function(self, context):
         compute_nodes = db.get_compute_node_stats(context)
@@ -114,7 +143,7 @@ class LoadBalancer(object):
         LOG.debug(_(memory_td))
         for node in compute_nodes:
             cpu_used_percent = node['cpu_used_percent']
-            memory_used = node['memory_total'] - node['memory_free']
+            memory_used = node['memory_used']
             memory_used_percent = round(
                 (float(memory_used) / float(node['memory_total'])) * 100.00, 0
             )
@@ -124,7 +153,17 @@ class LoadBalancer(object):
                 instances = db.get_instances_stat(
                     context,
                     node.compute_node.hypervisor_hostname)
-                self._choose_instance_to_migrate(instances, compute_nodes)
+                norm_ins = self._choose_instance_to_migrate(instances,
+                                                            compute_nodes)
+                hosts = self.host_manager.get_all_host_states(context)
+                expected_attrs = ['info_cache', 'security_groups',
+                                  'system_metadata']
+                instance = objects.Instance.get_by_uuid(context,
+                                                        '0a46c56c-dea3-4440-9e4a-41c3ae2516eb', expected_attrs)
+                # im = self.image_api.get(context, instance.get('image_ref'))
+                LOG.debug(_(instance.__dict__))
+                # LOG.debug(_(im))
+                LOG.debug(_(list(hosts)))
 
     def indicate_threshold(self, context):
         return self._step_threshold_function(context)
